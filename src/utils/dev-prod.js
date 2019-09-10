62fb2dc7c7f0ba7cf4c6627e433a36e70975090d
EvgenijMerchansky <evgenij.merchansky@gmail.com>
2019-09-10 13:57:16 +0300

    fix: bug with env

--- src/utils/dev-prod.js
+++ src/utils/dev-prod.js
@@ -1,3 +1,3 @@
-export const isProd = process.env.NODE_ENV === 'dev';
+export const isProd = process.env.NODE_ENV === 'development';
 export const devAndProd = (dev, prod) => (isProd ? prod : dev);
 export const isDev = !isProd;
